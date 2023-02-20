f28f44f81e8b13480fa4b0bf6a253f67668b168b
cmcelduff <111883363+cmcelduff@users.noreply.github.com>
2023-02-20 20:43:49 +0000

    First changes

---
+++ test.py
@@ -0,0 +1,31 @@
+# Import required libraries
+import traceback
+
+import requests
+import json
+import os
+
+
+
+NAME = "Dublin"
+STATIONS = "https://api.jcdecaux.com/vls/v1/stations"
+API_KEY = "7f06972a5ed335cf697379627fd13027274927c7"
+
+def main():
+    while True:
+        try:
+            r = requests.get(STATIONS, params={"apiKey": API_KEY, "contract": NAME})
+
+            store(json.loads(r.text))
+
+            time.sleep(5*60)
+        except:
+            print(traceback.format_exc())
+    return
+
+# print(r.status_code)
+#
+# load = json.loads(r.text)
+# print(load)
+# print(r.text)
+# print(r.url)
