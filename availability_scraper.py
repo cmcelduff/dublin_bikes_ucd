92cb9f786ae597689ad024dc52516d178b5bebea
cmcelduff <111883363+cmcelduff@users.noreply.github.com>
2023-02-23 17:02:59 +0000

    New scraper

---
+++ availability_scraper.py
@@ -0,0 +1,59 @@
+import sqlalchemy as sqla 
+from sqlalchemy import create_engine 
+import traceback 
+import glob 
+import os 
+import requests 
+import time 
+import json
+from IPython.display import display
+import pymysql
+import requests
+import traceback
+import datetime
+import time
+import os
+
+
+URI="dbbikes.cjk4ybuxtkwv.us-east-1.rds.amazonaws.com"
+PORT="3306"
+DB="dbbikes"
+USER="cmcelduff"
+PASSWORD="Tullamore1!"
+STATIONS="https://api.jcdecaux.com/vls/v1/stations"
+DubBike_API = "7f06972a5ed335cf697379627fd13027274927c7"
+NAME="Dublin"
+
+#Connect to database
+engine = create_engine("mysql+pymysql://{0}:{1}@{2}:{3}".format(USER, PASSWORD, URI, PORT), echo=True) 
+connection = engine.connect()
+
+def availability_to_db(text):
+    stations = json.loads(text)
+    #print(availability)
+    for station in stations:
+        #print(station.get("name"))
+        #print(station.get("number"))
+        #print(station.get("available_bikes"))
+        #print(station.get("available_bike_stands"))
+        #print(station.get("last_update"))
+        vals = (int(station.get("number")), int(station.get("available_bikes")), int(station.get("available_bike_stands")), int(station.get("last_update")))
+        engine.execute("INSERT INTO `dublin_bikes`.`availability` values(%s,%s,%s,%s)", vals)
+    return
+
+def main():
+    print(os.path)
+    while True:
+        try:
+            now = datetime.datetime.now()
+            r = requests.get(STATIONS, params={"apiKey": DubBike_API, "contract" : NAME})
+            print(r, now)
+            availability_to_db(r.text)
+            time.sleep(5*60)
+        except:
+            print(traceback.format_exc())
+            
+        return
+
+if __name__== "__main__":
+    main()
