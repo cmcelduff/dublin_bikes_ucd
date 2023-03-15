ee5dfc7678cd3948e4a773a888f689e4edabcd6c
ptr555 <123461801+ptr555@users.noreply.github.com>
2023-03-15 16:51:07 +0000

    wether image py file

--- weatherimages.py
+++ weatherimages.py
@@ -1,1 +1,18 @@
 #text to images
+import requests
+import json
+import datetime as dt
+
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+API_KEY = "13a747c8462fe1d8b7126b190b48a8ae"
+CITY = "Dublin, IE"
+
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+r = requests.get(url).json()
+
+#showing all values
+status = r['weather'][0]['description']
+print(status)
+
+
+
